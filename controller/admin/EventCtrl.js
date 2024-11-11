00c06a1c8aa6994e90f6837eb2ae373c13dce8d7
Abdul rahib <rahibrahib6740@gmail.com>
2024-11-11 18:20:47 +0530

    event model and control updation

---
+++ controller/admin/EventCtrl.js
@@ -0,0 +1,53 @@
+const Event=require('../../model/EventSchema');
+const Employee=require('../../model/userModel');
+const Cron=require('node-cron');
+
+
+const AddEventPage=async(req,res)=>{
+    try {
+        // const Event=await Event.find().populate('employee');
+        // const Employee=await Employee.find();
+        res.render('admin/addEvents',{Event,Employee})
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({success:false,message:'Internal server error'});
+    }
+}
+
+Cron.schedule('0 0 * * *',async ()=>{
+    const now=Date.now();
+    try {
+        const result=await Event.deleteMany({expirationtime:{$lt:now}});
+        console.log(`${result.deletedCount} expired Event deleted success`);
+    } catch (error) {
+        console.error('Error Expired Event deleting',error);
+    }
+});
+
+const AddEvent=async(req,res)=>{
+    try {
+        const {place,date,reportingTime,exitTime,jobTitle,
+            jobDescription,employerLimit,expirationTime} = req.body;
+
+            console.log(place,date,reportingTime,exitTime,jobTitle,
+                jobDescription,employerLimit,expirationTime)
+
+            const newEvent=new Event({
+                place:place,
+                date:date,
+                reportingTime:reportingTime,
+                exitTime:exitTime,
+                jobTitle:jobTitle,
+                jobDescription:jobDescription,
+                employerLimit:employerLimit,
+                expirationTime:expirationTime
+            })
+            await newEvent.save();
+            res.status(200).json({success:true, message:'new Event successfully created'});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({success:false,message:'Internal server error'});
+    }
+}
+
+module.exports={AddEvent,AddEventPage};
---
+++ model/EventSchema.js
@@ -0,0 +1,40 @@
+const mongoose=require('mongoose');
+
+const EventSchema=new mongoose.Schema({
+    place:{
+        type:String,
+        required:true,
+    },
+    date:{
+        type:Date,
+        reuired:true,
+    },
+    reportingTime:{
+        type:String,
+        required:true
+    },
+    exitTime:{
+        type:String,
+        required:true,
+    },
+    jobTitle:{
+        type:String,
+        required:true
+    },
+    jobDescription:{
+        type:String,
+        required:true,
+    },
+    employerLimit:{
+        type:String,
+        required:true,
+        default:0,
+        min:0
+    },
+    expirationTime:{
+        type:Date,
+        required:true
+    }
+}, {timestamps:true});
+
+module.exports=mongoose.model('events',EventSchema);
