089901287ad4843c023fa7e01b067a92740d4dea
Abdul rahib <rahibrahib6740@gmail.com>
2024-11-22 14:29:37 +0530

    cron deletrion

--- controller/AdminCtrl/EventCtrl.js
+++ controller/AdminCtrl/EventCtrl.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Event = require("../../model/EventSchema");
-const Cron = require("node-cron");
 
 const AddEventPage = async (req, res) => {
   try {
@@ -44,14 +43,12 @@ const EventdetailsPage = async (req, res) => {
         .json({ success: false, message: "Invalid event id" });
     }
 
-    // Find the event and populate currentEmployers
     const event = await Event.findById(eventId).populate(
-      "currentEmployers", // Field to populate
-      "name userId" // Fields to include from the employees collection
+      "currentEmployers", 
+      "name userId" 
     );
 
     if (event) {
-      // Format the event's date
       const date = new Date(event.date);
       event.formattedDate = date.toLocaleString("en-IN", {
         weekday: "short",
@@ -63,7 +60,6 @@ const EventdetailsPage = async (req, res) => {
         hour12: true,
       });
 
-      // Format the event's expiration time
       if (event.expirationTime) {
         const expirationTime = new Date(event.expirationTime);
         event.formattedexpirationTime = expirationTime.toLocaleString(
@@ -81,7 +77,6 @@ const EventdetailsPage = async (req, res) => {
       }
     }
 
-    // Render the page with the event and populated employee details
     res.render("admin/eventDetailsPage", { event });
   } catch (error) {
     console.error(error.message);
@@ -106,23 +101,6 @@ const EditEventPage = async (req, res) => {
   }
 };
 
-Cron.schedule("0 0 * * *", async () => {
-  const now = Date.now();
-  console.log(
-    `cron job is working is running at :${new Date(now).toLocaleString()}`
-  );
-
-  try {
-    const expiredEvent = await Event.find({ expirationTime: { $lt: now } });
-    console.log(expiredEvent);
-
-    const result = await Event.deleteMany({ expirationTime: { $lt: now } });
-
-    console.log(`${result.deletedCount} expired Event deleted success`);
-  } catch (error) {
-    console.error("Error Expired Event deleting", error);
-  }
-});
 const AddEvent = async (req, res) => {
   const adminId = req.session.userDataId;
   try {
@@ -147,7 +125,6 @@ const AddEvent = async (req, res) => {
       employerLimit: employerLimit,
       expirationTime: formattedExpirationTime,
      
-      // expirationTime: new Date(expirationTime)
     });
   } catch (error) {
     console.error(error.message);
@@ -195,12 +172,11 @@ const EditEvent = async (req, res) => {
 
 const DeleteEvent = async (req, res) => {
   try {
-    const eventId = req.params.id; // Extract the employee ID from the URL
+    const eventId = req.params.id; 
     if (!eventId) {
       return res.status(400).json({ error: "Invalid event ID" });
     }
 
-    // Find and delete the employee
     const deletedEvent = await Event.findByIdAndDelete(eventId);
 
     if (!deletedEvent) {
