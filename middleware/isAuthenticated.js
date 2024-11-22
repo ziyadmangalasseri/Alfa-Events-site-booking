131849fb266f399301896d8af09194abb8d5f4fb
ziyad <ziyadmuhammad9696@gmail.com>
2024-11-22 16:01:51 +0530

    add isAuthenticated middleware

---
+++ middleware/isAuthenticated.js
@@ -0,0 +1,9 @@
+ const isAuthenticated = (req, res, next) => {
+  if (req.session.isLoggedIn) {
+    next(); // Proceed to the next middleware or route handler
+  } else {
+    res.redirect("/"); // Redirect to login page if not logged in
+  }
+};
+
+module.exports  = {isAuthenticated}
